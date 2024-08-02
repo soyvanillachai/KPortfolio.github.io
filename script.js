7ad665489a2d0f5ec383d7f954090b1e68452324
soyvanillachai <khytownes@gmail.com>
2024-08-01 22:52:29 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
